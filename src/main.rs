1290ed3f8ff2c3a8acfce1838d9bb82dfdbe2b8d
maple8192 <57043077+maple8192@users.noreply.github.com>
2023-02-15 01:13:42 +0900

    :sparkles: ソースファイルの中身を読み取る #3

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,18 @@
 mod env_args;
+mod file_reader;
 
 use std::env;
 
 fn main() {
-    let error = env_args::check_args(&env::args().collect::<Vec<String>>());
-    match error {
-        Some(e) => println!("Error occurred: {}", e),
-        None => println!("Successfully!!")
+    let args = env::args().collect::<Vec<String>>();
+
+    let error = env_args::check_args(&args);
+    if let Some(e) = error {
+        println!("Error occurred: {}", e);
+        return;
     }
+
+    let src_path = args[1].clone();
+    let src = file_reader::read(src_path);
+    println!("{}", src);
 }
