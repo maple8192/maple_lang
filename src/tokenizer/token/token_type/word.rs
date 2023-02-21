fae2054d549cc0211ecd90f530a338f24ccc42cd
maple8192 <57043077+maple8192@users.noreply.github.com>
2023-02-21 21:59:53 +0900

    :sparkles: トークンを追加 #12

--- src/tokenizer/token/token_type/word.rs
+++ src/tokenizer/token/token_type/word.rs
@@ -1,18 +1,39 @@
 #[derive(Copy, Clone, Eq, PartialEq)]
 pub enum Word {
     Function,
+    If,
+    For,
+    While,
+    Loop,
+    Int,
+    Float,
+    String,
 }
 
 impl Word {
     pub fn to_str(&self) -> &str {
         match self {
             Word::Function => "fn",
+            Word::If => "if",
+            Word::For => "for",
+            Word::While => "while",
+            Word::Loop => "loop",
+            Word::Int => "int",
+            Word::Float => "float",
+            Word::String => "str",
         }
     }
 
     pub fn get_len_order_list() -> Vec<Self> {
         vec![
             Word::Function,
+            Word::If,
+            Word::For,
+            Word::While,
+            Word::Loop,
+            Word::Int,
+            Word::Float,
+            Word::String,
         ]
     }
 }
